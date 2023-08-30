58724f81165be459ad33d2e81e136e7d804ff81a
Nohiria Velazquez <nohiriavg@gmail.com>
2023-08-29 19:08:02 -0500

    Ejercicio 2 Guia Herencia Interface

---
+++ Herencia_Guia_Ejercicio_Interface/src/herencia_guia_ejercicio_interface/Herencia_Guia_Ejercicio_Interface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_guia_ejercicio_interface;
+
+/**
+ *
+ * @author nohyv
+ */
+public class Herencia_Guia_Ejercicio_Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyInterface myObject= new MyClass();
+        myObject.myMethod();
+    }
+    
+}
---
+++ Herencia_Guia_Ejercicio_Interface/src/herencia_guia_ejercicio_interface/MyClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_guia_ejercicio_interface;
+
+/**
+ *
+ * @author nohyv
+ */
+public class MyClass implements MyInterface{
+
+    @Override
+    public void myMethod() {
+        System.out.println("Implementing myMethod");
+    }
+    
+}
